3e71897cf7f8da3d88bcce75f36bbf7b669d720e
Rhys van der Waerden <rhys.vdw@gmail.com>
2023-03-06 00:50:48 +1100

    Add namespace Spiney.SubclassSelector
    To prevent name collision in global namespace

--- Attributes/SubclassSelectorAttribute.cs
+++ Attributes/SubclassSelectorAttribute.cs
@@ -1,91 +1,94 @@
 using System;
 
-/// <summary>
-/// Attribute that allows you to easily select from among the subclsses of a non-Unity Object, polymorphic reference value.
-/// Supports both singular fields and collections of any type that Odin supports.
-/// </summary>
-[System.Diagnostics.Conditional("UNITY_EDITOR")]
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
-public sealed class SubclassSelectorAttribute : Attribute
+namespace Spiney.SubclassSelector
 {
-	#region Constructor
-
-	public SubclassSelectorAttribute() { }
-
-	#endregion
-
-	#region Internal Members
-
-	private string _customTypeFilter = string.Empty;
-
-	private bool _hasCustomTypeFilter = false;
-
-	#endregion
-
-	#region Properties
-
 	/// <summary>
-	/// If false, duplicate won't be selectable in the collection selector, or collection element dropdowns.
+	/// Attribute that allows you to easily select from among the subclsses of a non-Unity Object, polymorphic reference value.
+	/// Supports both singular fields and collections of any type that Odin supports.
 	/// </summary>
-	/// <remarks>
-	/// Only affects collections. True by default.
-	/// </remarks>
-	public bool AllowDuplicates { get; set; } = true;
-
-	/// <summary>
-	/// Will hide the Odin default object reference picker if true as it it has been 
-	/// decorated with <see cref="Sirenix.OdinInspector.HideReferenceObjectPickerAttribute"/>.
-	/// </summary>
-	/// <remarks>
-	/// True by default.
-	/// </remarks>
-	public bool HideReferencePicker { get; set; } = true;
-
-	/// <summary>
-	/// If true, <see cref="Sirenix.OdinInspector.TypeFilterAttribute"/> dropdowns will be drawn at the top 
-	/// of each list element.
-	/// </summary>
-	/// <remarks>
-	/// Only affects collections. False by default.
-	/// </remarks>
-	public bool DrawDropdownForListElements { get; set; } = false;
-
-	/// <summary>
-	/// If true, will draw a BoxGroup-style box around each element of a collection.
-	/// </summary>
-	/// <remarks>
-	/// False by default. Only affects collections.
-	/// </remarks>
-	public bool DrawBoxForListElements { get; set; } = false;
-
-	/// <summary>
-	/// If true, will hide the class' label as if it had been decorated with <see cref="Sirenix.OdinInspector.HideLabelAttribute"/>.
-	/// </summary>
-	/// <remarks>
-	/// Only affects non-collection fields. True by default.
-	/// </remarks>
-	public bool HideClassLabel { get; set; } = true;
-
-	/// <summary>
-	/// Provide a value resolver expression to filter types after the intial collection of subtypes has been generated.
-	/// </summary>
-	/// <remarks>
-	/// Abstract types are already filtered.
-	/// </remarks>
-	public string CustomTypeFilter
+	[System.Diagnostics.Conditional("UNITY_EDITOR")]
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+	public sealed class SubclassSelectorAttribute : Attribute
 	{
-		get => _customTypeFilter;
-		set
+		#region Constructor
+
+		public SubclassSelectorAttribute() { }
+
+		#endregion
+
+		#region Internal Members
+
+		private string _customTypeFilter = string.Empty;
+
+		private bool _hasCustomTypeFilter = false;
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// If false, duplicate won't be selectable in the collection selector, or collection element dropdowns.
+		/// </summary>
+		/// <remarks>
+		/// Only affects collections. True by default.
+		/// </remarks>
+		public bool AllowDuplicates { get; set; } = true;
+
+		/// <summary>
+		/// Will hide the Odin default object reference picker if true as it it has been
+		/// decorated with <see cref="Sirenix.OdinInspector.HideReferenceObjectPickerAttribute"/>.
+		/// </summary>
+		/// <remarks>
+		/// True by default.
+		/// </remarks>
+		public bool HideReferencePicker { get; set; } = true;
+
+		/// <summary>
+		/// If true, <see cref="Sirenix.OdinInspector.TypeFilterAttribute"/> dropdowns will be drawn at the top
+		/// of each list element.
+		/// </summary>
+		/// <remarks>
+		/// Only affects collections. False by default.
+		/// </remarks>
+		public bool DrawDropdownForListElements { get; set; } = false;
+
+		/// <summary>
+		/// If true, will draw a BoxGroup-style box around each element of a collection.
+		/// </summary>
+		/// <remarks>
+		/// False by default. Only affects collections.
+		/// </remarks>
+		public bool DrawBoxForListElements { get; set; } = false;
+
+		/// <summary>
+		/// If true, will hide the class' label as if it had been decorated with <see cref="Sirenix.OdinInspector.HideLabelAttribute"/>.
+		/// </summary>
+		/// <remarks>
+		/// Only affects non-collection fields. True by default.
+		/// </remarks>
+		public bool HideClassLabel { get; set; } = true;
+
+		/// <summary>
+		/// Provide a value resolver expression to filter types after the intial collection of subtypes has been generated.
+		/// </summary>
+		/// <remarks>
+		/// Abstract types are already filtered.
+		/// </remarks>
+		public string CustomTypeFilter
 		{
-			_hasCustomTypeFilter = !string.IsNullOrEmpty(value);
-			_customTypeFilter = value;
+			get => _customTypeFilter;
+			set
+			{
+				_hasCustomTypeFilter = !string.IsNullOrEmpty(value);
+				_customTypeFilter = value;
+			}
 		}
+
+		/// <summary>
+		/// Is there a custom type filter expression?
+		/// </summary>
+		public bool HasCustomTypeFilter => _hasCustomTypeFilter;
+
+		#endregion
 	}
-
-	/// <summary>
-	/// Is there a custom type filter expression?
-	/// </summary>
-	public bool HasCustomTypeFilter => _hasCustomTypeFilter;
-
-	#endregion
 }
--- Editor/SubclassSelectorUtilities.cs
+++ Editor/SubclassSelectorUtilities.cs
@@ -10,269 +10,272 @@ using Sirenix.OdinInspector.Editor.ValueResolvers;
 using Sirenix.Utilities;
 using Sirenix.Utilities.Editor;
 
-/// <summary>
-/// Utility methods for use with <see cref="SubclassSelectorAttributeProcessor{T}"/>.
-/// </summary>
-public static class SubclassSelectorUtilities
+namespace Spiney.SubclassSelector
 {
-	#region Resolver Strings
-
-	public static string OpenSubclassSelectorString => $"@{nameof(SubclassSelectorUtilities)}.{nameof(OpenSubclassCollectionSelector)}($property)";
-
-	public static string TypeFilterResolverString => $"@{nameof(SubclassSelectorUtilities)}.{nameof(GetSubclassSelectorDropdownItems)}($property)";
-
-	public static string OnBeginBoxSubclassElementString => $"@{nameof(SubclassSelectorUtilities)}.{nameof(BeginDrawBoxedSubclassElement)}($property, $index)";
-
-	public static string OnEndBoxSubclassElementString => $"@{nameof(SubclassSelectorUtilities)}.{nameof(EndDrawBoxedSubclassElement)}()";
-
-	#endregion
-
-	#region Validation and Data Retreival
-
 	/// <summary>
-	/// Gets all child types of <paramref name="baseType"/>, filtering abstract types.
+	/// Utility methods for use with <see cref="SubclassSelectorAttributeProcessor{T}"/>.
 	/// </summary>
-	/// <param name="baseType">Type to get all derived/child types of.</param>
-	public static IEnumerable<Type> GetTypeDerivedTypes(Type baseType, InspectorProperty inspectorProperty)
+	public static class SubclassSelectorUtilities
 	{
-		var subclassSelector = inspectorProperty.Attributes.GetAttribute<SubclassSelectorAttribute>();
-		ValueResolver<bool> typeFilterResolver = null;
+		#region Resolver Strings
 
-		if (subclassSelector.HasCustomTypeFilter)
+		public static string OpenSubclassSelectorString => $"@{nameof(SubclassSelectorUtilities)}.{nameof(OpenSubclassCollectionSelector)}($property)";
+
+		public static string TypeFilterResolverString => $"@{nameof(SubclassSelectorUtilities)}.{nameof(GetSubclassSelectorDropdownItems)}($property)";
+
+		public static string OnBeginBoxSubclassElementString => $"@{nameof(SubclassSelectorUtilities)}.{nameof(BeginDrawBoxedSubclassElement)}($property, $index)";
+
+		public static string OnEndBoxSubclassElementString => $"@{nameof(SubclassSelectorUtilities)}.{nameof(EndDrawBoxedSubclassElement)}()";
+
+		#endregion
+
+		#region Validation and Data Retreival
+
+		/// <summary>
+		/// Gets all child types of <paramref name="baseType"/>, filtering abstract types.
+		/// </summary>
+		/// <param name="baseType">Type to get all derived/child types of.</param>
+		public static IEnumerable<Type> GetTypeDerivedTypes(Type baseType, InspectorProperty inspectorProperty)
 		{
-			typeFilterResolver = ValueResolver.Get<bool>(inspectorProperty, subclassSelector.CustomTypeFilter, 
-				new NamedValue("type", typeof(Type)));
+			var subclassSelector = inspectorProperty.Attributes.GetAttribute<SubclassSelectorAttribute>();
+			ValueResolver<bool> typeFilterResolver = null;
 
-			if (typeFilterResolver.HasError)
+			if (subclassSelector.HasCustomTypeFilter)
 			{
-				Debug.LogError(typeFilterResolver.ErrorMessage);
-			}
-		}
+				typeFilterResolver = ValueResolver.Get<bool>(inspectorProperty, subclassSelector.CustomTypeFilter,
+					new NamedValue("type", typeof(Type)));
 
-		var types = TypeCache.GetTypesDerivedFrom(baseType).Where(AbstractTypeFilter);
-
-		if (!baseType.IsAbstract && PassesCustomTypeFilter(baseType))
-		{
-			yield return baseType;
-		}
-
-		foreach (Type type in types)
-		{
-			if (PassesCustomTypeFilter(type))
-			{
-				yield return type;
-			}
-		}
-
-		// local methods
-		static bool AbstractTypeFilter(Type type) => !type.IsAbstract;
-
-		bool PassesCustomTypeFilter(Type type)
-		{
-			if (typeFilterResolver == null || typeFilterResolver.HasError)
-			{
-				return true;
-			}
-			else
-			{
-				typeFilterResolver.Context.NamedValues.Set("type", type);
-				bool result = typeFilterResolver.GetValue();
-				return result;
-			}
-		}
-	}
-
-	public static string GetTypeSubclassPath(Type type)
-	{
-		SubclassPathAttribute pathAttribute = type.GetAttribute<SubclassPathAttribute>();
-
-		if (pathAttribute == null || (!pathAttribute.HasSubClassPath && !pathAttribute.HasSubClassName))
-		{
-			return type.GetNiceName();
-		}
-
-		string path = string.Empty;
-		if (pathAttribute.HasSubClassPath)
-		{
-			path += (pathAttribute.SubClassPath + "/");
-		}
-
-		path += pathAttribute.HasSubClassName ? pathAttribute.SubClassName : type.Name;
-
-		return path;
-	}
-
-	/// <summary>
-	/// Returns whether a particular type exists with children of an <see cref="InspectorProperty"/>.
-	/// </summary>
-	/// <param name="derivedAreDuplicates">If true, types derived from <paramref name="type"/> will also count.</param>
-	public static bool TypeExistsInPropertyChildren(PropertyChildren children, Type type)
-	{
-		foreach (var child in children)
-		{
-			Type valueType = child.ValueEntry.TypeOfValue;
-			bool duplicate = (valueType == type);
-
-			if (duplicate)
-			{
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	#endregion
-
-	#region Field Methods
-
-	public static IEnumerable<ValueDropdownItem<Type>> GetSubclassSelectorDropdownItems(InspectorProperty property)
-	{
-		SubclassSelectorAttribute subclassSelector = property.Attributes.GetAttribute<SubclassSelectorAttribute>();
-		IEnumerable<Type> types = GetTypeDerivedTypes(property.ValueEntry.BaseValueType, property);
-
-		InspectorProperty parent = property.ParentValueProperty;
-		bool parentIsCollection = parent != null && parent.ChildResolver is ICollectionResolver;
-
-		// if the parent is not a collection,
-		// or we don't care about duplicate values...
-		if (!parentIsCollection || subclassSelector.AllowDuplicates)
-		{
-			foreach (var type in types)
-			{
-				yield return GetDropdownItem(type);
-			}
-		}
-		else
-		{
-			foreach (var type in types)
-			{
-				// the current type should be selectable so that the dropdown
-				// draws the correct Subclass path
-				if (type == property.ValueEntry.TypeOfValue)
+				if (typeFilterResolver.HasError)
 				{
-					yield return GetDropdownItem(type);
-					continue;
-				}
-
-				if (!TypeExistsInPropertyChildren(parent.Children, type))
-				{
-					yield return GetDropdownItem(type);
+					Debug.LogError(typeFilterResolver.ErrorMessage);
 				}
 			}
-		}
 
-		static ValueDropdownItem<Type> GetDropdownItem(Type type)
-		{
-			string name = GetTypeSubclassPath(type);
-			return new ValueDropdownItem<Type>(name, type);
-		}
-	}
+			var types = TypeCache.GetTypesDerivedFrom(baseType).Where(AbstractTypeFilter);
 
-	#endregion
-
-	#region Collection Methods
-
-	public static void OpenSubclassCollectionSelector(InspectorProperty property)
-	{
-		var collectionResolver = (ICollectionResolver)property.ChildResolver;
-		var selectorAttribute = property.Attributes.GetAttribute<SubclassSelectorAttribute>();
-
-		Type elementType = collectionResolver.ElementType;
-		IEnumerable<Type> types = GetTypeDerivedTypes(collectionResolver.ElementType, property).Where(GenericSelectorTypeFilter);
-
-		List<GenericSelectorItem<Type>> selectorTypes = new List<GenericSelectorItem<Type>>();
-		foreach (Type type in types)
-		{
-			string name = GetTypeSubclassPath(type);
-			var selectorItem = new GenericSelectorItem<Type>(name, type);
-
-			selectorTypes.Add(selectorItem);
-		}
-
-		GenericSelector<Type> selector = new GenericSelector<Type>(title: "Select Types", true, selectorTypes);
-
-		foreach (var menuItem in selector.SelectionTree.EnumerateTree())
-		{
-			// skip menu item if it has children (a dropdown) or has a null value
-			if (menuItem.ChildMenuItems.Count > 0 && menuItem.Value == null)
+			if (!baseType.IsAbstract && PassesCustomTypeFilter(baseType))
 			{
-				continue;
+				yield return baseType;
 			}
 
-			Type itemType = (Type)menuItem.Value;
-			bool hasDefaultConstructor = itemType.GetConstructor(Type.EmptyTypes) != null;
-
-			if (!hasDefaultConstructor)
+			foreach (Type type in types)
 			{
-				menuItem.Icon = EditorIcons.AlertTriangle.Raw;
-				menuItem.Name = $"{menuItem.Name} - No Default Constructor";
-			}
-			else
-			{
-				menuItem.Icon = EditorIcons.File.Raw;
-			}
-		}
-
-		selector.DrawConfirmSelectionButton = true;
-		selector.SelectionConfirmed += AddSelectedTypes;
-		selector.ShowInPopup();
-
-		// local methods
-		bool GenericSelectorTypeFilter(Type type)
-		{
-			if (selectorAttribute.AllowDuplicates)
-			{
-				return true;
-			}
-
-			return !TypeExistsInPropertyChildren(property.Children, type);
-		}
-
-		void AddSelectedTypes(IEnumerable<Type> selectedTypes)
-		{
-			foreach (var selection in selectedTypes)
-			{
-				// prevent errors when selecting dropdown values
-				if (selection == null)
+				if (PassesCustomTypeFilter(type))
 				{
-					continue;
+					yield return type;
 				}
+			}
 
-				bool hasDefaultConstructor = selection.GetConstructor(Type.EmptyTypes) != null;
-				object[] values;
+			// local methods
+			static bool AbstractTypeFilter(Type type) => !type.IsAbstract;
 
-				if (hasDefaultConstructor)
+			bool PassesCustomTypeFilter(Type type)
+			{
+				if (typeFilterResolver == null || typeFilterResolver.HasError)
 				{
-					var instance = Activator.CreateInstance(selection);
-					values = new object[] { instance };
+					return true;
 				}
 				else
 				{
-					var instance = Sirenix.Serialization.UnitySerializationUtility.CreateDefaultUnityInitializedObject(selection);
-					values = new object[] { instance };
+					typeFilterResolver.Context.NamedValues.Set("type", type);
+					bool result = typeFilterResolver.GetValue();
+					return result;
 				}
-
-				collectionResolver.QueueAdd(values);
-				property.Children.Update();
 			}
 		}
-	}
 
-	public static void BeginDrawBoxedSubclassElement(InspectorProperty property, int index)
-	{
-		var child = property.Children[index];
-		var subclassPath = child.ValueEntry.TypeOfValue.GetAttribute<SubclassPathAttribute>();
-		string groupName = subclassPath?.SubClassName ?? child.ValueEntry.TypeOfValue.GetNiceName();
-		groupName = groupName.Replace('.', '-');
-		SirenixEditorGUI.BeginBox(groupName, centerLabel: true);
-	}
+		public static string GetTypeSubclassPath(Type type)
+		{
+			SubclassPathAttribute pathAttribute = type.GetAttribute<SubclassPathAttribute>();
 
-	public static void EndDrawBoxedSubclassElement()
-	{
-		SirenixEditorGUI.EndBox();
-	}
+			if (pathAttribute == null || (!pathAttribute.HasSubClassPath && !pathAttribute.HasSubClassName))
+			{
+				return type.GetNiceName();
+			}
 
-	#endregion
+			string path = string.Empty;
+			if (pathAttribute.HasSubClassPath)
+			{
+				path += (pathAttribute.SubClassPath + "/");
+			}
+
+			path += pathAttribute.HasSubClassName ? pathAttribute.SubClassName : type.Name;
+
+			return path;
+		}
+
+		/// <summary>
+		/// Returns whether a particular type exists with children of an <see cref="InspectorProperty"/>.
+		/// </summary>
+		/// <param name="derivedAreDuplicates">If true, types derived from <paramref name="type"/> will also count.</param>
+		public static bool TypeExistsInPropertyChildren(PropertyChildren children, Type type)
+		{
+			foreach (var child in children)
+			{
+				Type valueType = child.ValueEntry.TypeOfValue;
+				bool duplicate = (valueType == type);
+
+				if (duplicate)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		#endregion
+
+		#region Field Methods
+
+		public static IEnumerable<ValueDropdownItem<Type>> GetSubclassSelectorDropdownItems(InspectorProperty property)
+		{
+			SubclassSelectorAttribute subclassSelector = property.Attributes.GetAttribute<SubclassSelectorAttribute>();
+			IEnumerable<Type> types = GetTypeDerivedTypes(property.ValueEntry.BaseValueType, property);
+
+			InspectorProperty parent = property.ParentValueProperty;
+			bool parentIsCollection = parent != null && parent.ChildResolver is ICollectionResolver;
+
+			// if the parent is not a collection,
+			// or we don't care about duplicate values...
+			if (!parentIsCollection || subclassSelector.AllowDuplicates)
+			{
+				foreach (var type in types)
+				{
+					yield return GetDropdownItem(type);
+				}
+			}
+			else
+			{
+				foreach (var type in types)
+				{
+					// the current type should be selectable so that the dropdown
+					// draws the correct Subclass path
+					if (type == property.ValueEntry.TypeOfValue)
+					{
+						yield return GetDropdownItem(type);
+						continue;
+					}
+
+					if (!TypeExistsInPropertyChildren(parent.Children, type))
+					{
+						yield return GetDropdownItem(type);
+					}
+				}
+			}
+
+			static ValueDropdownItem<Type> GetDropdownItem(Type type)
+			{
+				string name = GetTypeSubclassPath(type);
+				return new ValueDropdownItem<Type>(name, type);
+			}
+		}
+
+		#endregion
+
+		#region Collection Methods
+
+		public static void OpenSubclassCollectionSelector(InspectorProperty property)
+		{
+			var collectionResolver = (ICollectionResolver)property.ChildResolver;
+			var selectorAttribute = property.Attributes.GetAttribute<SubclassSelectorAttribute>();
+
+			Type elementType = collectionResolver.ElementType;
+			IEnumerable<Type> types = GetTypeDerivedTypes(collectionResolver.ElementType, property).Where(GenericSelectorTypeFilter);
+
+			List<GenericSelectorItem<Type>> selectorTypes = new List<GenericSelectorItem<Type>>();
+			foreach (Type type in types)
+			{
+				string name = GetTypeSubclassPath(type);
+				var selectorItem = new GenericSelectorItem<Type>(name, type);
+
+				selectorTypes.Add(selectorItem);
+			}
+
+			GenericSelector<Type> selector = new GenericSelector<Type>(title: "Select Types", true, selectorTypes);
+
+			foreach (var menuItem in selector.SelectionTree.EnumerateTree())
+			{
+				// skip menu item if it has children (a dropdown) or has a null value
+				if (menuItem.ChildMenuItems.Count > 0 && menuItem.Value == null)
+				{
+					continue;
+				}
+
+				Type itemType = (Type)menuItem.Value;
+				bool hasDefaultConstructor = itemType.GetConstructor(Type.EmptyTypes) != null;
+
+				if (!hasDefaultConstructor)
+				{
+					menuItem.Icon = EditorIcons.AlertTriangle.Raw;
+					menuItem.Name = $"{menuItem.Name} - No Default Constructor";
+				}
+				else
+				{
+					menuItem.Icon = EditorIcons.File.Raw;
+				}
+			}
+
+			selector.DrawConfirmSelectionButton = true;
+			selector.SelectionConfirmed += AddSelectedTypes;
+			selector.ShowInPopup();
+
+			// local methods
+			bool GenericSelectorTypeFilter(Type type)
+			{
+				if (selectorAttribute.AllowDuplicates)
+				{
+					return true;
+				}
+
+				return !TypeExistsInPropertyChildren(property.Children, type);
+			}
+
+			void AddSelectedTypes(IEnumerable<Type> selectedTypes)
+			{
+				foreach (var selection in selectedTypes)
+				{
+					// prevent errors when selecting dropdown values
+					if (selection == null)
+					{
+						continue;
+					}
+
+					bool hasDefaultConstructor = selection.GetConstructor(Type.EmptyTypes) != null;
+					object[] values;
+
+					if (hasDefaultConstructor)
+					{
+						var instance = Activator.CreateInstance(selection);
+						values = new object[] { instance };
+					}
+					else
+					{
+						var instance = Sirenix.Serialization.UnitySerializationUtility.CreateDefaultUnityInitializedObject(selection);
+						values = new object[] { instance };
+					}
+
+					collectionResolver.QueueAdd(values);
+					property.Children.Update();
+				}
+			}
+		}
+
+		public static void BeginDrawBoxedSubclassElement(InspectorProperty property, int index)
+		{
+			var child = property.Children[index];
+			var subclassPath = child.ValueEntry.TypeOfValue.GetAttribute<SubclassPathAttribute>();
+			string groupName = subclassPath?.SubClassName ?? child.ValueEntry.TypeOfValue.GetNiceName();
+			groupName = groupName.Replace('.', '-');
+			SirenixEditorGUI.BeginBox(groupName, centerLabel: true);
+		}
+
+		public static void EndDrawBoxedSubclassElement()
+		{
+			SirenixEditorGUI.EndBox();
+		}
+
+		#endregion
+	}
 }
 #endif
